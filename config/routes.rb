d3b5e826a243a6d1ac6ad3fc2ce705d3ed3390f0
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-03-02 03:20:23 +0000

    nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
 
   # get("/", { :controller => "boards", :action => "index" })
   # more robust way to define root url - devise gem
-  root to: "board#index"
-
+  root to: "boards#index"
 
   # Routes for the Post resource:
 
