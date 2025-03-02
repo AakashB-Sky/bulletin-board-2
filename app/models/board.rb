86ba1995dd91ba5559c81669cbd09b783cd0c3d6
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-03-02 03:50:38 +0000

    defined association between users and boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -10,4 +10,7 @@
 #
 class Board < ApplicationRecord
   validates(:name, presence: true, uniqueness: true)
+  validates(:user_id, presence: true)
+
+  belongs_to(:user, class_name: "User", foreign_key: "user_id", primary_key: "id")
 end
